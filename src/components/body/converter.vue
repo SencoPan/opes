173ef4da47f532e4bcd8b1bd37414de8fc08826b
SencoPan <salabdmaxjoy@gmail.com>
2020-03-29 02:19:29 +0300

    Body converter templating

--- src/components/body/converter.vue
+++ src/components/body/converter.vue
@@ -1,32 +1,36 @@
 <template>
   <div class="converter">
-    <div class="hr">
+    <!--    <div class="hr">
       <hr />
-    </div>
+    </div>-->
     <div class="input">
-      <div class="current_currency">
+      <div class="current_currency part">
+        <div class="current_currency_display">
+          <p>RUB</p>
+        </div>
         <label>
-          <p class=""></p>
-          text
-          <input type="text" />
+          <input type="text" class="currency_amount" />
         </label>
       </div>
-      <div class="convert_sign">
-        <input type="submit" />
+      <div class="convert_sign part">
+        <input type="submit" class="exchange-action" />
+        <i class="fa fa-exchange-alt"></i>
       </div>
-      <div class="converted_currency">
+      <div class="converted_currency part">
+        <div class="current_currency_display">
+          <p>USD</p>
+        </div>
         <label>
-          text
-          <input type="text" />
+          <input type="text" class="currency_amount" />
         </label>
       </div>
     </div>
     <div class="submit">
-      <p>Test 2</p>
+      <p>Manual convert</p>
     </div>
-    <div class="hr">
+    <!--    <div class="hr">
       <hr />
-    </div>
+    </div>-->
   </div>
 </template>
 
@@ -49,18 +53,17 @@ export default {
 .input div,
 label {
   display: flex;
-  flex-direction: row;
 }
-.input div:nth-child(1) {
+.input div.part:nth-child(1) {
   margin-left: auto;
 }
-.input div:nth-child(2) {
+.input div.part:nth-child(2) {
   margin: 0 3em 0 3em;
 }
-.input div:nth-child(3) {
+.input div.part:nth-child(3) {
   margin-right: auto;
 }
-hr {
+/*hr {
   height: 1px;
   border: none;
   color: #0080ff;
@@ -69,6 +72,30 @@ hr {
 }
 .hr {
   display: flex;
+}*/
+.exchange-action {
+  display: none;
+}
+.exchange-action + svg {
+  display: flex;
+  font-size: 3em;
+  margin: auto 0 auto 0;
+}
+.current_currency, .converted_currency {
+  flex-direction: column;
+}
+.current_currency_display {
+  display: flex;
+  margin: 1em auto 1em auto;
+  font-size: 1.2em;
+}
+.current_currency_display p {
+  margin: auto 0 auto 0;
+}
+.currency_amount {
+  max-width: 3.5em;
+  font-size: 3.5em;
+  padding: 0.1em;
 }
 .input {
 }
